bed69c03ed9c7affcb0b1da5bbca15b44e53d518
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2024-03-17 16:41:38 +0300

    Create Base Controller

--- Elasticsearch.API/Repositories/ProductRepository.cs
+++ Elasticsearch.API/Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DevopsTest.Models;
+using Elasticsearch.API.DTOs;
 using Nest;
 using System.Collections.Immutable;
 
@@ -19,6 +20,47 @@ namespace Elasticsearch.API.Repositories
             var result = await _client.SearchAsync<Product>(s => s.Index(IndexName).Query(q => q.MatchAll()));
             foreach (var hit in result.Hits) hit.Source.Id = hit.Id;
             return result.Documents.ToImmutableList();
+        }
+
+        public async Task<Product?> GetByIdAsync(string id)
+        {
+            var response = await _client.GetAsync<Product>(id, x => x.Index(IndexName));
+
+            if (!response.IsValid)
+            {
+                return null;
+
+            }
+            response.Source.Id = response.Id;
+            return response.Source;
+        }
+
+        public async Task<Product?> SaveAsync (Product newProduct)
+        {
+            newProduct.Created = DateTime.Now;
+            var response = await _client.IndexAsync(newProduct, x => x.Index(IndexName));
+
+            if (!response.IsValid) return null;
+
+            newProduct.Id = response.Id;
+
+            return newProduct;
+        }
+
+        public async Task<bool> UpdateAsync(ProductUpdateDto updateProduct)
+        {
+            var response = await _client.UpdateAsync<Product, ProductUpdateDto>(updateProduct.Id, x => x.Index(IndexName).Doc(updateProduct));
+
+            if (!response.IsValid) return false;
+
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(string Id)
+        {
+            var response = await _client.DeleteAsync<Product>(Id, x => x.Index(IndexName));
+
+            return response.IsValid;
         }
     }
 }
